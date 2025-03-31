7c6395a46b8e494209bf9ac3127ba5f4db24cb53
Serge01700 <waxqo69@gmail.com>
2025-03-31 15:28:29 +0200

    y

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,31 @@
-var splide = new Splide( '.splide', {
-    direction: 'ttb',
-    height   : '10rem',
-    wheel    : true,
-  } );
+document.addEventListener('DOMContentLoaded', function() {
+    let splide = new Splide('.splide', {
+      direction: 'ttb',
+      perPage: 2, 
+      height: '750px',
+      wheel: true,
+      type: 'loop',
+      pagination: false,
+      autoplay: true,
+      interval: 2700,
+      arrows: false,
+      pauseOnHover:true
+    });
   
-  splide.mount();
+    splide.mount();
+  
+    let splide2 = new Splide('.splide2', {
+      direction: 'rtl',
+      perPage: 2, 
+      height: '750px',
+      wheel: true,
+      type: 'loop',
+      pagination: false,
+      autoplay: true,
+      interval: 1700,
+      arrows: false,
+    });
+  
+    splide2.mount();
+  });
+  
