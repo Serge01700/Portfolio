b3c931a328ec9085ceb1b429f80ea87e02d2b5da
Serge01700 <waxqo69@gmail.com>
2025-04-02 16:47:18 +0200

    m

--- assets/js/splide.js
+++ assets/js/splide.js
@@ -1,30 +1,16 @@
 const swiper = new Swiper('.swiper', {
-    
-    direction:'horizontal',
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    // speed: 50000,
-    slidesPerView: 2,
-    loop: true,
+  direction: 'vertical',
+  spaceBetween: 205, 
+  slidesPerView: 2,
+  // loop: true, 
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false, 
+  },
+
+  pagination: {
+    dynamicBullets: true, 
+  },
+
   
-    
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
- 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
- 
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+});
